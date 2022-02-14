525dd6ca6d5cca655e1cc9961d15316e4452f304
RsosoR <godpkpk205@gmail.com>
2022-02-14 11:40:08 +0800

    PmsProduct

--- ec-admin/src/main/java/com/rsosor/ec/controller/PmsProductController.java
+++ ec-admin/src/main/java/com/rsosor/ec/controller/PmsProductController.java
@@ -1,10 +1,18 @@
 package com.rsosor.ec.controller;
 
+import com.rsosor.ec.common.api.CommonPage;
+import com.rsosor.ec.common.api.CommonResult;
+import com.rsosor.ec.dto.PmsProductParam;
+import com.rsosor.ec.dto.PmsProductQueryParam;
+import com.rsosor.ec.dto.PmsProductResult;
+import com.rsosor.ec.model.PmsProduct;
 import com.rsosor.ec.service.PmsProductService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 商品管理 Controller
@@ -20,6 +28,109 @@ public class PmsProductController {
     @Autowired
     private PmsProductService productService;
 
+    @ApiOperation("創建商品")
+    @PostMapping(value = "/create")
+    public CommonResult create(@RequestBody PmsProductParam productParam) {
+        int count = productService.create(productParam);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
 
+    @ApiOperation("根據商品 id 獲取商品編輯訊息")
+    @GetMapping(value = "/updateInfo/{id}")
+    public CommonResult<PmsProductResult> getUpdateInfo(@PathVariable Long id) {
+        PmsProductResult productResult = productService.getUpdateInfo(id);
+        return CommonResult.success(productResult);
+    }
 
+    @ApiOperation("更新商品")
+    @PostMapping(value = "/update/{id}")
+    public CommonResult update(@PathVariable Long id, @RequestBody PmsProductParam productParam) {
+        int count = productService.update(id, productParam);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("查詢商品")
+    @GetMapping(value = "/list")
+    public CommonResult<CommonPage<PmsProduct>> getList(PmsProductQueryParam productQueryParam,
+                                                 @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+                                                 @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
+        List<PmsProduct> productList = productService.list(productQueryParam, pageSize, pageNum);
+        return CommonResult.success(CommonPage.restPage(productList));
+    }
+
+    @ApiOperation("根據商品名稱或貨號模糊查詢")
+    @GetMapping(value = "/simpleList")
+    public CommonResult<List<PmsProduct>> getList(String keyword) {
+        List<PmsProduct> productList = productService.list(keyword);
+        return CommonResult.success(productList);
+    }
+
+    @ApiOperation("批量修改審核狀態")
+    @PostMapping(value = "/update/verifyStatus")
+    public CommonResult updateVerifyStatus(@RequestParam("ids") List<Long> ids,
+                                           @RequestParam("verifyStatus") Integer verifyStatus,
+                                           @RequestParam("detail") String detail) {
+        int count = productService.updateVerifyStatus(ids, verifyStatus, detail);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("批量上下架商品")
+    @PostMapping(value = "/update/publishStatus")
+    public CommonResult updatePublishStatus(@RequestParam("ids") List<Long> ids,
+                                            @RequestParam("publishStatus") Integer publishStatus) {
+        int count = productService.updatePublishStatus(ids, publishStatus);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("批量推薦商品")
+    @PostMapping(value = "/update/recommendStatus")
+    public CommonResult updateRecommendStatus(@RequestParam("ids") List<Long> ids,
+                                              @RequestParam("recommendStatus") Integer recommendStatus) {
+        int count = productService.updateRecommendStatus(ids, recommendStatus);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("批量設為新品")
+    @PostMapping(value = "/update/newStatus")
+    public CommonResult updateNewStatus(@RequestParam("ids") List<Long> ids,
+                                        @RequestParam("newStatus") Integer newStatus) {
+        int count = productService.updateNewStatus(ids, newStatus);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("批量修改刪除狀態")
+    @PostMapping(value = "/update/deleteStatus")
+    public CommonResult updateDeleteStatus(@RequestParam("ids") List<Long> ids,
+                                           @RequestParam("deleteStatus") Integer deleteStatus) {
+        int count = productService.updateDeleteStatus(ids, deleteStatus);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
 }
